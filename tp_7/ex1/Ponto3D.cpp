127ba83f1cf660a12a2fcc465fbce35834f3a8fe
Matheus Vinicius <matheusvinicius.mv939@gmail.com>
2022-01-19 02:37:55 -0300

    finish

---
+++ tp_7/ex1/Ponto3D.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Ponto3D.h"
+
+using namespace std;
+
+ostream& operator<< (ostream &op, const Ponto2D &p){
+            op << endl;
+            op << "x = " << p.x << endl;
+            op << "y = " << p.y << endl;
+            return op;
+}
+
+Ponto2D& Ponto2D::operator= (const Ponto2D &p){
+    x = p.x;
+    y = p.y;
+    return *this;
+}
+
+ostream &operator<< (ostream &output, const Ponto3D &p3){
+    output << endl;
+    double x_ = p3.get_x(), y_ = p3.get_y();
+    output << "x = " << x_ << endl;
+    output << "y = " << y_ << endl;
+    output << "z = " << p3.z << endl;
+    return output;
+}
+
+Ponto3D &Ponto3D::operator= (const Ponto2D &p2){
+    /*double x_= 0, y_ = 0;
+    x_ = p2.get_x();
+    y_ = p2.get_y();
+    z = z;
+    set(x_, y_);*/
+    set(p2.get_x(), p2.get_y());
+    z = z;
+
+    return *this;
+}
---
+++ tp_7/ex1/Ponto3D.h
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#ifndef PONTO3D_H
+#define PONTO3D_H
+
+using namespace std;
+
+class Ponto2D{
+    
+     public:
+        Ponto2D(int xx = 0.0, int yy = 0.0):x(xx),y(yy){ };
+        friend ostream& operator<< (ostream &op, const Ponto2D &p);
+        Ponto2D& operator= (const Ponto2D &p);
+        ~Ponto2D(){};
+        double get_x(void) const{ return x; }
+        double get_y(void) const{ return y; }
+        void set(double nx, double ny) { x=nx; y=ny; }
+    
+    private:                
+        double x;
+        double y;
+};
+
+// a ser implementada
+class Ponto3D: public Ponto2D{
+    
+    public:
+        Ponto3D(double xx = 0, double yy = 0, double zz=0):Ponto2D(xx,yy),z(zz){};
+        friend ostream& operator<< (ostream &op, const Ponto3D &p);
+        Ponto3D& operator= (const Ponto2D &p);
+        void set(double nx = 0, double ny = 0, double nz=0){Ponto2D(nx, ny); z = nz;}
+        double get_z(){return z;};
+            
+    private:
+        double z;
+};    
+
+#endif
